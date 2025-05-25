98bbc77f32804b60899f61ac1aa7ffe47391d24e
Paul Ifemene <ifemenep@gmail.com>
2025-05-25 09:57:18 +0300

    Update question_four.py

--- question_four.py
+++ question_four.py
@@ -290,3 +290,296 @@ water_units_paid_for = water(amount_paid_for_water)
 # Print/display the amount of unit for YAKA and water respectively
 print (f"You have successfully subscribed for:",yaka_units_paid_for,f"YAKA units")
 print (f"You have successfully subscribed for:",water_units_paid_for,f"Water units")
+
+# YAKA and water Calculator
+# YAKA unit function
+def yaka(amount_paid_y):
+    one_y_unit = 560
+    anount_paid_y = int()
+    yaka_units = amount_paid_y/one_y_unit
+    return yaka_units
+
+
+
+# Water unit function
+def water(amount_paid_w):
+    one_w_unit = 4100
+    anount_paid_w = int()
+    water_units = amount_paid_w/one_w_unit
+    return water_units
+
+
+# Using the both functions in a program
+print(f"Welcome to YAKA and Water Subscription")
+# Getting the value of amount paid by users
+amount_paid_for_yaka = int(input("Please enter the amount you want to pay for YAKA: "))
+amount_paid_for_water = int(input("Please enter the amount you want to pay for Water: "))
+
+# Using the value of the amount paid and the functions to determine how many units of YAKA and water respectively
+yaka_units_paid_for = yaka(amount_paid_for_yaka)
+water_units_paid_for = water(amount_paid_for_water)
+
+# Print/display the amount of unit for YAKA and water respectively
+print (f"You have successfully subscribed for:",yaka_units_paid_for,f"YAKA units")
+print (f"You have successfully subscribed for:",water_units_paid_for,f"Water units")
+
+# YAKA and water Calculator
+# YAKA unit function
+def yaka(amount_paid_y):
+    one_y_unit = 560
+    anount_paid_y = int()
+    yaka_units = amount_paid_y/one_y_unit
+    return yaka_units
+
+
+
+# Water unit function
+def water(amount_paid_w):
+    one_w_unit = 4100
+    anount_paid_w = int()
+    water_units = amount_paid_w/one_w_unit
+    return water_units
+
+
+# Using the both functions in a program
+print(f"Welcome to YAKA and Water Subscription")
+# Getting the value of amount paid by users
+amount_paid_for_yaka = int(input("Please enter the amount you want to pay for YAKA: "))
+amount_paid_for_water = int(input("Please enter the amount you want to pay for Water: "))
+
+# Using the value of the amount paid and the functions to determine how many units of YAKA and water respectively
+yaka_units_paid_for = yaka(amount_paid_for_yaka)
+water_units_paid_for = water(amount_paid_for_water)
+
+# Print/display the amount of unit for YAKA and water respectively
+print (f"You have successfully subscribed for:",yaka_units_paid_for,f"YAKA units")
+print (f"You have successfully subscribed for:",water_units_paid_for,f"Water units")
+
+# YAKA and water Calculator
+# YAKA unit function
+def yaka(amount_paid_y):
+    one_y_unit = 560
+    anount_paid_y = int()
+    yaka_units = amount_paid_y/one_y_unit
+    return yaka_units
+
+
+
+# Water unit function
+def water(amount_paid_w):
+    one_w_unit = 4100
+    anount_paid_w = int()
+    water_units = amount_paid_w/one_w_unit
+    return water_units
+
+
+# Using the both functions in a program
+print(f"Welcome to YAKA and Water Subscription")
+# Getting the value of amount paid by users
+amount_paid_for_yaka = int(input("Please enter the amount you want to pay for YAKA: "))
+amount_paid_for_water = int(input("Please enter the amount you want to pay for Water: "))
+
+# Using the value of the amount paid and the functions to determine how many units of YAKA and water respectively
+yaka_units_paid_for = yaka(amount_paid_for_yaka)
+water_units_paid_for = water(amount_paid_for_water)
+
+# Print/display the amount of unit for YAKA and water respectively
+print (f"You have successfully subscribed for:",yaka_units_paid_for,f"YAKA units")
+print (f"You have successfully subscribed for:",water_units_paid_for,f"Water units")
+
+
+# YAKA and water Calculator
+# YAKA unit function
+def yaka(amount_paid_y):
+    one_y_unit = 560
+    anount_paid_y = int()
+    yaka_units = amount_paid_y/one_y_unit
+    return yaka_units
+
+
+
+# Water unit function
+def water(amount_paid_w):
+    one_w_unit = 4100
+    anount_paid_w = int()
+    water_units = amount_paid_w/one_w_unit
+    return water_units
+
+
+# Using the both functions in a program
+print(f"Welcome to YAKA and Water Subscription")
+# Getting the value of amount paid by users
+amount_paid_for_yaka = int(input("Please enter the amount you want to pay for YAKA: "))
+amount_paid_for_water = int(input("Please enter the amount you want to pay for Water: "))
+
+# Using the value of the amount paid and the functions to determine how many units of YAKA and water respectively
+yaka_units_paid_for = yaka(amount_paid_for_yaka)
+water_units_paid_for = water(amount_paid_for_water)
+
+# Print/display the amount of unit for YAKA and water respectively
+print (f"You have successfully subscribed for:",yaka_units_paid_for,f"YAKA units")
+print (f"You have successfully subscribed for:",water_units_paid_for,f"Water units")
+
+
+# YAKA and water Calculator
+# YAKA unit function
+def yaka(amount_paid_y):
+    one_y_unit = 560
+    anount_paid_y = int()
+    yaka_units = amount_paid_y/one_y_unit
+    return yaka_units
+
+
+
+# Water unit function
+def water(amount_paid_w):
+    one_w_unit = 4100
+    anount_paid_w = int()
+    water_units = amount_paid_w/one_w_unit
+    return water_units
+
+
+# Using the both functions in a program
+print(f"Welcome to YAKA and Water Subscription")
+# Getting the value of amount paid by users
+amount_paid_for_yaka = int(input("Please enter the amount you want to pay for YAKA: "))
+amount_paid_for_water = int(input("Please enter the amount you want to pay for Water: "))
+
+# Using the value of the amount paid and the functions to determine how many units of YAKA and water respectively
+yaka_units_paid_for = yaka(amount_paid_for_yaka)
+water_units_paid_for = water(amount_paid_for_water)
+
+# Print/display the amount of unit for YAKA and water respectively
+print (f"You have successfully subscribed for:",yaka_units_paid_for,f"YAKA units")
+print (f"You have successfully subscribed for:",water_units_paid_for,f"Water units")
+
+
+# YAKA and water Calculator
+# YAKA unit function
+def yaka(amount_paid_y):
+    one_y_unit = 560
+    anount_paid_y = int()
+    yaka_units = amount_paid_y/one_y_unit
+    return yaka_units
+
+
+
+# Water unit function
+def water(amount_paid_w):
+    one_w_unit = 4100
+    anount_paid_w = int()
+    water_units = amount_paid_w/one_w_unit
+    return water_units
+
+
+# Using the both functions in a program
+print(f"Welcome to YAKA and Water Subscription")
+# Getting the value of amount paid by users
+amount_paid_for_yaka = int(input("Please enter the amount you want to pay for YAKA: "))
+amount_paid_for_water = int(input("Please enter the amount you want to pay for Water: "))
+
+# Using the value of the amount paid and the functions to determine how many units of YAKA and water respectively
+yaka_units_paid_for = yaka(amount_paid_for_yaka)
+water_units_paid_for = water(amount_paid_for_water)
+
+# Print/display the amount of unit for YAKA and water respectively
+print (f"You have successfully subscribed for:",yaka_units_paid_for,f"YAKA units")
+print (f"You have successfully subscribed for:",water_units_paid_for,f"Water units")
+
+# YAKA and water Calculator
+# YAKA unit function
+def yaka(amount_paid_y):
+    one_y_unit = 560
+    anount_paid_y = int()
+    yaka_units = amount_paid_y/one_y_unit
+    return yaka_units
+
+
+
+# Water unit function
+def water(amount_paid_w):
+    one_w_unit = 4100
+    anount_paid_w = int()
+    water_units = amount_paid_w/one_w_unit
+    return water_units
+
+
+# Using the both functions in a program
+print(f"Welcome to YAKA and Water Subscription")
+# Getting the value of amount paid by users
+amount_paid_for_yaka = int(input("Please enter the amount you want to pay for YAKA: "))
+amount_paid_for_water = int(input("Please enter the amount you want to pay for Water: "))
+
+# Using the value of the amount paid and the functions to determine how many units of YAKA and water respectively
+yaka_units_paid_for = yaka(amount_paid_for_yaka)
+water_units_paid_for = water(amount_paid_for_water)
+
+# Print/display the amount of unit for YAKA and water respectively
+print (f"You have successfully subscribed for:",yaka_units_paid_for,f"YAKA units")
+print (f"You have successfully subscribed for:",water_units_paid_for,f"Water units")
+
+
+# YAKA and water Calculator
+# YAKA unit function
+def yaka(amount_paid_y):
+    one_y_unit = 560
+    anount_paid_y = int()
+    yaka_units = amount_paid_y/one_y_unit
+    return yaka_units
+
+
+
+# Water unit function
+def water(amount_paid_w):
+    one_w_unit = 4100
+    anount_paid_w = int()
+    water_units = amount_paid_w/one_w_unit
+    return water_units
+
+
+# Using the both functions in a program
+print(f"Welcome to YAKA and Water Subscription")
+# Getting the value of amount paid by users
+amount_paid_for_yaka = int(input("Please enter the amount you want to pay for YAKA: "))
+amount_paid_for_water = int(input("Please enter the amount you want to pay for Water: "))
+
+# Using the value of the amount paid and the functions to determine how many units of YAKA and water respectively
+yaka_units_paid_for = yaka(amount_paid_for_yaka)
+water_units_paid_for = water(amount_paid_for_water)
+
+# Print/display the amount of unit for YAKA and water respectively
+print (f"You have successfully subscribed for:",yaka_units_paid_for,f"YAKA units")
+print (f"You have successfully subscribed for:",water_units_paid_for,f"Water units")
+
+
+# YAKA and water Calculator
+# YAKA unit function
+def yaka(amount_paid_y):
+    one_y_unit = 560
+    anount_paid_y = int()
+    yaka_units = amount_paid_y/one_y_unit
+    return yaka_units
+
+
+
+# Water unit function
+def water(amount_paid_w):
+    one_w_unit = 4100
+    anount_paid_w = int()
+    water_units = amount_paid_w/one_w_unit
+    return water_units
+
+
+# Using the both functions in a program
+print(f"Welcome to YAKA and Water Subscription")
+# Getting the value of amount paid by users
+amount_paid_for_yaka = int(input("Please enter the amount you want to pay for YAKA: "))
+amount_paid_for_water = int(input("Please enter the amount you want to pay for Water: "))
+
+# Using the value of the amount paid and the functions to determine how many units of YAKA and water respectively
+yaka_units_paid_for = yaka(amount_paid_for_yaka)
+water_units_paid_for = water(amount_paid_for_water)
+
+# Print/display the amount of unit for YAKA and water respectively
+print (f"You have successfully subscribed for:",yaka_units_paid_for,f"YAKA units")
+print (f"You have successfully subscribed for:",water_units_paid_for,f"Water units")
