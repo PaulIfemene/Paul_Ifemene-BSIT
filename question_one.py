02757b8a3bbc2a23a2df8415cafc8d8b04d8571a
Paul Ifemene <ifemenep@gmail.com>
2025-06-09 02:55:33 +0300

    Update question_one.py

--- question_one.py
+++ question_one.py
@@ -167,3 +167,347 @@ width = float(input("Enter the width of the Rectangle: "))
 perimeter = 2 * (lenght + width)
 # then lastly we print the perimeter of the rectangle
 print(f"The Perimeter of the rectangle is:",perimeter)
+
+
+
+# Area of a circle calculator
+# the costant value for Pi = 3.142 and is stored in the variable P
+P = 3.142
+# using the input function we get the value of the radius of a circle, and store it in the variable R
+R = float(input("Enter the radius of the circle: "))
+# Then we calculate the area using the values from variables P and R 
+Area_of_circle = P * (R*R)
+# Then lastly we print/display the Area of a circl from the variable Area_of_circle
+print(f"The area of the circle is:",Area_of_circle)
+
+# Perimeter of a Rectangle calculator
+# Firstl we get the lenght of the Rectamgle from the user and store it in the variable lenght
+lenght = float(input("Enter the lenght of the Rectangle: "))
+# then we also get the width of the Rectamgle from the user and store it in the variable width
+width = float(input("Enter the width of the Rectangle: "))
+# Then using the both values stored in the variable lenght and width we calculate the perimeter of the rectangle
+perimeter = 2 * (lenght + width)
+# then lastly we print the perimeter of the rectangle
+print(f"The Perimeter of the rectangle is:",perimeter)
+
+
+# Area of a circle calculator
+# the costant value for Pi = 3.142 and is stored in the variable P
+P = 3.142
+# using the input function we get the value of the radius of a circle, and store it in the variable R
+R = float(input("Enter the radius of the circle: "))
+# Then we calculate the area using the values from variables P and R 
+Area_of_circle = P * (R*R)
+# Then lastly we print/display the Area of a circl from the variable Area_of_circle
+print(f"The area of the circle is:",Area_of_circle)
+
+# Perimeter of a Rectangle calculator
+# Firstl we get the lenght of the Rectamgle from the user and store it in the variable lenght
+lenght = float(input("Enter the lenght of the Rectangle: "))
+# then we also get the width of the Rectamgle from the user and store it in the variable width
+width = float(input("Enter the width of the Rectangle: "))
+# Then using the both values stored in the variable lenght and width we calculate the perimeter of the rectangle
+perimeter = 2 * (lenght + width)
+# then lastly we print the perimeter of the rectangle
+print(f"The Perimeter of the rectangle is:",perimeter)
+
+
+# Area of a circle calculator
+# the costant value for Pi = 3.142 and is stored in the variable P
+P = 3.142
+# using the input function we get the value of the radius of a circle, and store it in the variable R
+R = float(input("Enter the radius of the circle: "))
+# Then we calculate the area using the values from variables P and R 
+Area_of_circle = P * (R*R)
+# Then lastly we print/display the Area of a circl from the variable Area_of_circle
+print(f"The area of the circle is:",Area_of_circle)
+
+# Perimeter of a Rectangle calculator
+# Firstl we get the lenght of the Rectamgle from the user and store it in the variable lenght
+lenght = float(input("Enter the lenght of the Rectangle: "))
+# then we also get the width of the Rectamgle from the user and store it in the variable width
+width = float(input("Enter the width of the Rectangle: "))
+# Then using the both values stored in the variable lenght and width we calculate the perimeter of the rectangle
+perimeter = 2 * (lenght + width)
+# then lastly we print the perimeter of the rectangle
+print(f"The Perimeter of the rectangle is:",perimeter)
+
+
+
+# Area of a circle calculator
+# the costant value for Pi = 3.142 and is stored in the variable P
+P = 3.142
+# using the input function we get the value of the radius of a circle, and store it in the variable R
+R = float(input("Enter the radius of the circle: "))
+# Then we calculate the area using the values from variables P and R 
+Area_of_circle = P * (R*R)
+# Then lastly we print/display the Area of a circl from the variable Area_of_circle
+print(f"The area of the circle is:",Area_of_circle)
+
+# Perimeter of a Rectangle calculator
+# Firstl we get the lenght of the Rectamgle from the user and store it in the variable lenght
+lenght = float(input("Enter the lenght of the Rectangle: "))
+# then we also get the width of the Rectamgle from the user and store it in the variable width
+width = float(input("Enter the width of the Rectangle: "))
+# Then using the both values stored in the variable lenght and width we calculate the perimeter of the rectangle
+perimeter = 2 * (lenght + width)
+# then lastly we print the perimeter of the rectangle
+print(f"The Perimeter of the rectangle is:",perimeter)
+
+
+
+# Area of a circle calculator
+# the costant value for Pi = 3.142 and is stored in the variable P
+P = 3.142
+# using the input function we get the value of the radius of a circle, and store it in the variable R
+R = float(input("Enter the radius of the circle: "))
+# Then we calculate the area using the values from variables P and R 
+Area_of_circle = P * (R*R)
+# Then lastly we print/display the Area of a circl from the variable Area_of_circle
+print(f"The area of the circle is:",Area_of_circle)
+
+# Perimeter of a Rectangle calculator
+# Firstl we get the lenght of the Rectamgle from the user and store it in the variable lenght
+lenght = float(input("Enter the lenght of the Rectangle: "))
+# then we also get the width of the Rectamgle from the user and store it in the variable width
+width = float(input("Enter the width of the Rectangle: "))
+# Then using the both values stored in the variable lenght and width we calculate the perimeter of the rectangle
+perimeter = 2 * (lenght + width)
+# then lastly we print the perimeter of the rectangle
+print(f"The Perimeter of the rectangle is:",perimeter)
+
+
+
+# Area of a circle calculator
+# the costant value for Pi = 3.142 and is stored in the variable P
+P = 3.142
+# using the input function we get the value of the radius of a circle, and store it in the variable R
+R = float(input("Enter the radius of the circle: "))
+# Then we calculate the area using the values from variables P and R 
+Area_of_circle = P * (R*R)
+# Then lastly we print/display the Area of a circl from the variable Area_of_circle
+print(f"The area of the circle is:",Area_of_circle)
+
+# Perimeter of a Rectangle calculator
+# Firstl we get the lenght of the Rectamgle from the user and store it in the variable lenght
+lenght = float(input("Enter the lenght of the Rectangle: "))
+# then we also get the width of the Rectamgle from the user and store it in the variable width
+width = float(input("Enter the width of the Rectangle: "))
+# Then using the both values stored in the variable lenght and width we calculate the perimeter of the rectangle
+perimeter = 2 * (lenght + width)
+# then lastly we print the perimeter of the rectangle
+print(f"The Perimeter of the rectangle is:",perimeter)
+
+
+# Area of a circle calculator
+# the costant value for Pi = 3.142 and is stored in the variable P
+P = 3.142
+# using the input function we get the value of the radius of a circle, and store it in the variable R
+R = float(input("Enter the radius of the circle: "))
+# Then we calculate the area using the values from variables P and R 
+Area_of_circle = P * (R*R)
+# Then lastly we print/display the Area of a circl from the variable Area_of_circle
+print(f"The area of the circle is:",Area_of_circle)
+
+# Perimeter of a Rectangle calculator
+# Firstl we get the lenght of the Rectamgle from the user and store it in the variable lenght
+lenght = float(input("Enter the lenght of the Rectangle: "))
+# then we also get the width of the Rectamgle from the user and store it in the variable width
+width = float(input("Enter the width of the Rectangle: "))
+# Then using the both values stored in the variable lenght and width we calculate the perimeter of the rectangle
+perimeter = 2 * (lenght + width)
+# then lastly we print the perimeter of the rectangle
+print(f"The Perimeter of the rectangle is:",perimeter)
+
+
+# Area of a circle calculator
+# the costant value for Pi = 3.142 and is stored in the variable P
+P = 3.142
+# using the input function we get the value of the radius of a circle, and store it in the variable R
+R = float(input("Enter the radius of the circle: "))
+# Then we calculate the area using the values from variables P and R 
+Area_of_circle = P * (R*R)
+# Then lastly we print/display the Area of a circl from the variable Area_of_circle
+print(f"The area of the circle is:",Area_of_circle)
+
+# Perimeter of a Rectangle calculator
+# Firstl we get the lenght of the Rectamgle from the user and store it in the variable lenght
+lenght = float(input("Enter the lenght of the Rectangle: "))
+# then we also get the width of the Rectamgle from the user and store it in the variable width
+width = float(input("Enter the width of the Rectangle: "))
+# Then using the both values stored in the variable lenght and width we calculate the perimeter of the rectangle
+perimeter = 2 * (lenght + width)
+# then lastly we print the perimeter of the rectangle
+print(f"The Perimeter of the rectangle is:",perimeter)
+
+
+
+# Area of a circle calculator
+# the costant value for Pi = 3.142 and is stored in the variable P
+P = 3.142
+# using the input function we get the value of the radius of a circle, and store it in the variable R
+R = float(input("Enter the radius of the circle: "))
+# Then we calculate the area using the values from variables P and R 
+Area_of_circle = P * (R*R)
+# Then lastly we print/display the Area of a circl from the variable Area_of_circle
+print(f"The area of the circle is:",Area_of_circle)
+
+# Perimeter of a Rectangle calculator
+# Firstl we get the lenght of the Rectamgle from the user and store it in the variable lenght
+lenght = float(input("Enter the lenght of the Rectangle: "))
+# then we also get the width of the Rectamgle from the user and store it in the variable width
+width = float(input("Enter the width of the Rectangle: "))
+# Then using the both values stored in the variable lenght and width we calculate the perimeter of the rectangle
+perimeter = 2 * (lenght + width)
+# then lastly we print the perimeter of the rectangle
+print(f"The Perimeter of the rectangle is:",perimeter)
+
+
+# Area of a circle calculator
+# the costant value for Pi = 3.142 and is stored in the variable P
+P = 3.142
+# using the input function we get the value of the radius of a circle, and store it in the variable R
+R = float(input("Enter the radius of the circle: "))
+# Then we calculate the area using the values from variables P and R 
+Area_of_circle = P * (R*R)
+# Then lastly we print/display the Area of a circl from the variable Area_of_circle
+print(f"The area of the circle is:",Area_of_circle)
+
+# Perimeter of a Rectangle calculator
+# Firstl we get the lenght of the Rectamgle from the user and store it in the variable lenght
+lenght = float(input("Enter the lenght of the Rectangle: "))
+# then we also get the width of the Rectamgle from the user and store it in the variable width
+width = float(input("Enter the width of the Rectangle: "))
+# Then using the both values stored in the variable lenght and width we calculate the perimeter of the rectangle
+perimeter = 2 * (lenght + width)
+# then lastly we print the perimeter of the rectangle
+print(f"The Perimeter of the rectangle is:",perimeter)
+
+
+# Area of a circle calculator
+# the costant value for Pi = 3.142 and is stored in the variable P
+P = 3.142
+# using the input function we get the value of the radius of a circle, and store it in the variable R
+R = float(input("Enter the radius of the circle: "))
+# Then we calculate the area using the values from variables P and R 
+Area_of_circle = P * (R*R)
+# Then lastly we print/display the Area of a circl from the variable Area_of_circle
+print(f"The area of the circle is:",Area_of_circle)
+
+# Perimeter of a Rectangle calculator
+# Firstl we get the lenght of the Rectamgle from the user and store it in the variable lenght
+lenght = float(input("Enter the lenght of the Rectangle: "))
+# then we also get the width of the Rectamgle from the user and store it in the variable width
+width = float(input("Enter the width of the Rectangle: "))
+# Then using the both values stored in the variable lenght and width we calculate the perimeter of the rectangle
+perimeter = 2 * (lenght + width)
+# then lastly we print the perimeter of the rectangle
+print(f"The Perimeter of the rectangle is:",perimeter)
+
+
+
+# Area of a circle calculator
+# the costant value for Pi = 3.142 and is stored in the variable P
+P = 3.142
+# using the input function we get the value of the radius of a circle, and store it in the variable R
+R = float(input("Enter the radius of the circle: "))
+# Then we calculate the area using the values from variables P and R 
+Area_of_circle = P * (R*R)
+# Then lastly we print/display the Area of a circl from the variable Area_of_circle
+print(f"The area of the circle is:",Area_of_circle)
+
+# Perimeter of a Rectangle calculator
+# Firstl we get the lenght of the Rectamgle from the user and store it in the variable lenght
+lenght = float(input("Enter the lenght of the Rectangle: "))
+# then we also get the width of the Rectamgle from the user and store it in the variable width
+width = float(input("Enter the width of the Rectangle: "))
+# Then using the both values stored in the variable lenght and width we calculate the perimeter of the rectangle
+perimeter = 2 * (lenght + width)
+# then lastly we print the perimeter of the rectangle
+print(f"The Perimeter of the rectangle is:",perimeter)
+
+
+
+# Area of a circle calculator
+# the costant value for Pi = 3.142 and is stored in the variable P
+P = 3.142
+# using the input function we get the value of the radius of a circle, and store it in the variable R
+R = float(input("Enter the radius of the circle: "))
+# Then we calculate the area using the values from variables P and R 
+Area_of_circle = P * (R*R)
+# Then lastly we print/display the Area of a circl from the variable Area_of_circle
+print(f"The area of the circle is:",Area_of_circle)
+
+# Perimeter of a Rectangle calculator
+# Firstl we get the lenght of the Rectamgle from the user and store it in the variable lenght
+lenght = float(input("Enter the lenght of the Rectangle: "))
+# then we also get the width of the Rectamgle from the user and store it in the variable width
+width = float(input("Enter the width of the Rectangle: "))
+# Then using the both values stored in the variable lenght and width we calculate the perimeter of the rectangle
+perimeter = 2 * (lenght + width)
+# then lastly we print the perimeter of the rectangle
+print(f"The Perimeter of the rectangle is:",perimeter)
+
+
+
+# Area of a circle calculator
+# the costant value for Pi = 3.142 and is stored in the variable P
+P = 3.142
+# using the input function we get the value of the radius of a circle, and store it in the variable R
+R = float(input("Enter the radius of the circle: "))
+# Then we calculate the area using the values from variables P and R 
+Area_of_circle = P * (R*R)
+# Then lastly we print/display the Area of a circl from the variable Area_of_circle
+print(f"The area of the circle is:",Area_of_circle)
+
+# Perimeter of a Rectangle calculator
+# Firstl we get the lenght of the Rectamgle from the user and store it in the variable lenght
+lenght = float(input("Enter the lenght of the Rectangle: "))
+# then we also get the width of the Rectamgle from the user and store it in the variable width
+width = float(input("Enter the width of the Rectangle: "))
+# Then using the both values stored in the variable lenght and width we calculate the perimeter of the rectangle
+perimeter = 2 * (lenght + width)
+# then lastly we print the perimeter of the rectangle
+print(f"The Perimeter of the rectangle is:",perimeter)
+
+
+# Area of a circle calculator
+# the costant value for Pi = 3.142 and is stored in the variable P
+P = 3.142
+# using the input function we get the value of the radius of a circle, and store it in the variable R
+R = float(input("Enter the radius of the circle: "))
+# Then we calculate the area using the values from variables P and R 
+Area_of_circle = P * (R*R)
+# Then lastly we print/display the Area of a circl from the variable Area_of_circle
+print(f"The area of the circle is:",Area_of_circle)
+
+# Perimeter of a Rectangle calculator
+# Firstl we get the lenght of the Rectamgle from the user and store it in the variable lenght
+lenght = float(input("Enter the lenght of the Rectangle: "))
+# then we also get the width of the Rectamgle from the user and store it in the variable width
+width = float(input("Enter the width of the Rectangle: "))
+# Then using the both values stored in the variable lenght and width we calculate the perimeter of the rectangle
+perimeter = 2 * (lenght + width)
+# then lastly we print the perimeter of the rectangle
+print(f"The Perimeter of the rectangle is:",perimeter)
+
+
+# Area of a circle calculator
+# the costant value for Pi = 3.142 and is stored in the variable P
+P = 3.142
+# using the input function we get the value of the radius of a circle, and store it in the variable R
+R = float(input("Enter the radius of the circle: "))
+# Then we calculate the area using the values from variables P and R 
+Area_of_circle = P * (R*R)
+# Then lastly we print/display the Area of a circl from the variable Area_of_circle
+print(f"The area of the circle is:",Area_of_circle)
+
+# Perimeter of a Rectangle calculator
+# Firstl we get the lenght of the Rectamgle from the user and store it in the variable lenght
+lenght = float(input("Enter the lenght of the Rectangle: "))
+# then we also get the width of the Rectamgle from the user and store it in the variable width
+width = float(input("Enter the width of the Rectangle: "))
+# Then using the both values stored in the variable lenght and width we calculate the perimeter of the rectangle
+perimeter = 2 * (lenght + width)
+# then lastly we print the perimeter of the rectangle
+print(f"The Perimeter of the rectangle is:",perimeter)
